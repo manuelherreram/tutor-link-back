1d76b36c2fdaa1ee655edb3a5987f5e9538c43c1
rofaba <rodrigofaure@gmail.com>
2024-05-09 15:36:08 -0400

    update BD

--- src/main/java/com/proyecto/tutorlink/entity/Image.java
+++ src/main/java/com/proyecto/tutorlink/entity/Image.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 @Table(name = "images")
 public class Image {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
--- src/main/java/com/proyecto/tutorlink/entity/Teacher.java
+++ src/main/java/com/proyecto/tutorlink/entity/Teacher.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Table(name = "teachers")
 public class Teacher {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
