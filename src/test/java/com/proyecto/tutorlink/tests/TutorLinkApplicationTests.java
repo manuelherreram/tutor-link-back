e8325ac16652b9b2034feceb0af98b8b9b2b747a
Manuel Herrera <manuel.herrera.m@gmail.com>
2024-05-23 14:57:43 -0400

    modifica application properties para MySQL, tambien test y prod

--- src/test/java/com/proyecto/tutorlink/tests/TutorLinkApplicationTests.java
+++ src/test/java/com/proyecto/tutorlink/tests/TutorLinkApplicationTests.java
@@ -1,6 +1,10 @@
 package com.proyecto.tutorlink.tests;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles(value="test")
 @SpringBootTest
 class TutorLinkApplicationTests {
     @Test
